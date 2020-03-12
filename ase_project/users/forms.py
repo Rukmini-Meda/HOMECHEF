507dad94ed8a31238728d5c60d23d55acfa935e8
TinLock7 <tintinprajju@gmail.com>
2020-03-12 17:27:19 +0530

    blank

--- ase_project/users/forms.py
+++ ase_project/users/forms.py
@@ -1,11 +1,12 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.forms import AuthenticationForm
 
 class UserRegisterForm(UserCreationForm):
-	email = forms.EmailField()
+	email = forms.EmailField
 	class Meta:
 		model = User
 		fields = ['username','first_name','last_name','email','password1','password2']
+
+
 
--- ase_project/users/models.py
+++ ase_project/users/models.py
@@ -1,4 +1,3 @@
 from django.db import models
-from django.conf import settings
-# Create your models here.
 
+# Create your models here.
