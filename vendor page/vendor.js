2dd27fca622b94ab6d5815e730548eea513da65a
sumasreeeduru <sumasreeeduru@gmail.com>
2020-03-12 15:52:24 +0530

    addedextrapages

--- vendor page/vendor.js
+++ vendor page/vendor.js
@@ -8,7 +8,3 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
 }
-
-
-
-
---
+++ vendor-slider/js/vendorslider.js
@@ -0,0 +1,15 @@
+$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  for (var i=0;i<4;i++) {
+    next=next.next();
+    if (!next.length) {
+      next=$(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
