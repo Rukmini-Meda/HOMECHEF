bf1c238a37e089f09e254b6f3394e5e52968dace
Srithanya <srithanyakomakula@gmail.com>
2020-11-07 01:27:36 +0530

    project folder

--- ase_project/users/forms.py
+++ ase_project/users/forms.py
@@ -5,6 +5,9 @@ from django.contrib.auth.forms import UserCreationForm
 class UserRegisterForm(UserCreationForm):
 	# name = forms.CharField()
 	email = forms.EmailField(label='Email Address')
+	# address=forms.CharField(max_length=100)
+	pincode=forms.IntegerField()
+
 	class Meta:
 		model = User
-		fields = ['username','email','password1','password2']
+		fields = ['username','email','pincode','password1','password2',]
